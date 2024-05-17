382f36fa47ccf53776a0d69011bb876abc0e876e
TheNeoHacker07 <tsstark225@gmail.com>
2024-05-17 16:45:53 +0600

    sdf

---
+++ taskapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Author,Book,Reader
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Reader)
+# Register your models here.
